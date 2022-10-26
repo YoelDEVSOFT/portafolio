fb2d453684679682b5d6e22ce43bb328cccc93c0
YoelDEVSOFT <YoelDEVSOFT@github.com>
2022-10-25 23:03:21 -0500

    commit inicial

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from sys import base_prefix
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'),
+    )
+
+    from  . import portfolio
+
+    app.register_blueprint(portfolio.bp)
+
+    return app
+
+
